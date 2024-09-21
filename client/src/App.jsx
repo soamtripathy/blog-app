a7f6926f2d682b44bdd7c8717abf3236ac99e3fa
soamtripathy <soumyaranjantripathy44@gmail.com>
2024-09-21 22:55:59 +0530

    frontend setup and navabar design

---
+++ client/src/App.jsx
@@ -0,0 +1,131 @@
+import "./App.css";
+import logoImage from "../public/morsamiya.jpg";
+
+function App() {
+  return (
+    <main>
+      <header>
+        <a href="" className="logo">
+          <img src={logoImage} /> 
+        </a>
+        <nav>
+          <a href="">Login</a>
+          <a href="">Register</a>
+        </nav>
+      </header>
+
+      <div className="post">
+        <div className="image">
+          <img
+            src="https://images.unsplash.com/photo-1434030216411-0b793f4b4173?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+            alt=""
+          />
+        </div>
+        <div className="texts">
+          <h2>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Illo
+            perferendis rerum esse nobis maxime, hic culpa. Ipsam rerum, sequi
+            ad facere esse consequatur quae hic.
+          </h2>
+          <p>
+            {" "}
+            Rerum nobis id, ab perferendis officiis autem molestias facere fugit
+            ratione? Ad rem mollitia sunt nam perferendis modi et temporibus
+            veniam asperiores dolore, obcaecati, recusandae placeat nulla odio
+            itaque odit at aperiam quasi unde minima.
+          </p>
+        </div>
+      </div>
+      <div className="post">
+        <div className="image">
+          <img
+            src="https://images.unsplash.com/photo-1434030216411-0b793f4b4173?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+            alt=""
+          />
+        </div>
+        <div className="texts">
+          <h2>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Illo
+            perferendis rerum esse nobis maxime, hic culpa. Ipsam rerum, sequi
+            ad facere esse consequatur quae hic.
+          </h2>
+          <p>
+            {" "}
+            Rerum nobis id, ab perferendis officiis autem molestias facere fugit
+            ratione? Ad rem mollitia sunt nam perferendis modi et temporibus
+            veniam asperiores dolore, obcaecati, recusandae placeat nulla odio
+            itaque odit at aperiam quasi unde minima.
+          </p>
+        </div>
+      </div>
+      <div className="post">
+        <div className="image">
+          <img
+            src="https://images.unsplash.com/photo-1434030216411-0b793f4b4173?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+            alt=""
+          />
+        </div>
+        <div className="texts">
+          <h2>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Illo
+            perferendis rerum esse nobis maxime, hic culpa. Ipsam rerum, sequi
+            ad facere esse consequatur quae hic.
+          </h2>
+          <p>
+            {" "}
+            Rerum nobis id, ab perferendis officiis autem molestias facere fugit
+            ratione? Ad rem mollitia sunt nam perferendis modi et temporibus
+            veniam asperiores dolore, obcaecati, recusandae placeat nulla odio
+            itaque odit at aperiam quasi unde minima.
+          </p>
+        </div>
+      </div>
+      <div className="post">
+        <div className="image">
+          <img
+            src="https://images.unsplash.com/photo-1434030216411-0b793f4b4173?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+            alt=""
+          />
+        </div>
+        <div className="texts">
+          <h2>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Illo
+            perferendis rerum esse nobis maxime, hic culpa. Ipsam rerum, sequi
+            ad facere esse consequatur quae hic.
+          </h2>
+          <p>
+            {" "}
+            Rerum nobis id, ab perferendis officiis autem molestias facere fugit
+            ratione? Ad rem mollitia sunt nam perferendis modi et temporibus
+            veniam asperiores dolore, obcaecati, recusandae placeat nulla odio
+            itaque odit at aperiam quasi unde minima.
+          </p>
+        </div>
+      </div>
+      <div className="post">
+        <div className="image">
+          <img
+            src="https://images.unsplash.com/photo-1434030216411-0b793f4b4173?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+            alt=""
+          />
+        </div>
+        <div className="texts">
+          <h2>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Illo
+            perferendis rerum esse nobis maxime, hic culpa. Ipsam rerum, sequi
+            ad facere esse consequatur quae hic.
+          </h2>
+          <p>
+            {" "}
+            Rerum nobis id, ab perferendis officiis autem molestias facere fugit
+            ratione? Ad rem mollitia sunt nam perferendis modi et temporibus
+            veniam asperiores dolore, obcaecati, recusandae placeat nulla odio
+            itaque odit at aperiam quasi unde minima.
+          </p>
+        </div>
+      </div>
+    </main>
+  );
+}
+
+export default App;
