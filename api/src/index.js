387b6845e657b75a06380599b347a0404b936654
soamtripathy <soumyaranjantripathy44@gmail.com>
2024-09-26 23:21:25 +0530

    done

--- api/src/index.js
+++ api/src/index.js
@@ -122,6 +122,36 @@ app.post("/post", uploadMiddleware.single("file"), async (req, res) => {
     res.status(500).json({ error: "Failed to create post" });
   }
 });
+app.put("/post", uploadMiddleware.single("file"), async (req, res) => {
+  let newPath = null;
+  if (req.file) {
+    const { originalname, path } = req.file;
+    const parts = originalname.split(".");
+    const ext = parts[parts.length - 1];
+    newPath = path + "." + ext;
+    fs.renameSync(path, newPath);
+  }
+
+  const { token } = req.cookies;
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, {}, async (err, info) => {
+    if (err) throw err;
+    const { id, title, summary, content } = req.body;
+    const postDoc = await Post.findById(id);
+    console.log(postDoc);
+    const isAuthor = JSON.stringify(postDoc.author) === JSON.stringify(info.id);
+    if (!isAuthor) {
+      return res.status(400).json("you are not the author");
+    }
+    postDoc.title = title;
+    postDoc.summary = summary;
+    postDoc.content = content;
+    postDoc.cover = newPath ? newPath : postDoc.cover;
+
+    await postDoc.save();
+
+    res.json(postDoc);
+  });
+});
 
 app.get("/post", async (req, res) => {
   try {
@@ -139,7 +169,7 @@ app.get("/post", async (req, res) => {
 app.get("/post/:id", async (req, res) => {
   const { id } = req.params;
   const postDoc = await Post.findById(id).populate("author", ["username"]);
-  console.log(postDoc)
+  console.log(postDoc);
   res.json(postDoc);
 });
 
